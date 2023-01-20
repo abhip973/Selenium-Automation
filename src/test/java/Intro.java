6d2c0f7a9a6aec5210b8ea6b00a34646bb14d4a0
abhipunj01 <abhishek.punj@dentsu.com>
2023-01-21 00:20:40 +0530

    Initial commit

---
+++ src/test/java/Intro.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+
+public class Intro {
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+//        System.setProperty("webdriver.edge.driver","src/drivers/msedgedriver.exe");
+//        WebDriver driver = new EdgeDriver();
+
+//        System.setProperty("webdriver.gecko.driver","src/drivers/geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://rahulshettyacademy.com/");
+        String title = driver.getTitle();
+        System.out.println(title);
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
+
+    }
+}
