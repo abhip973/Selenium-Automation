a558affe715d1a0b605dadbafb2d0b113b8870ae
abhipunj01 <abhishek.punj@dentsu.com>
2023-01-23 23:07:51 +0530

    Dropdown

---
+++ src/test/java/DynamicDropdown.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DynamicDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.xpath("(//span[contains(@id,'originStation')])[1]")).click();
+        Thread.sleep(2000);
+
+    }
+}
