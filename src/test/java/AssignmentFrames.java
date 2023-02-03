1138f21fc7361d5a687f0f030cdb7f6ceb9dc224
abhipunj01 <abhishek.punj@dentsu.com>
2023-02-03 12:53:34 +0530

    Frames and child window

---
+++ src/test/java/AssignmentFrames.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AssignmentFrames {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/");
+
+        driver.findElement(By.xpath("//a[text()='Nested Frames']")).click();
+
+        //Control is on top frame
+        driver.switchTo().frame(driver.findElement(By.cssSelector("frame[name='frame-top']")));
+
+        //Control is moved to inner frame
+        driver.switchTo().frame(driver.findElement(By.cssSelector("frame[name='frame-middle']")));
+
+        //Gets text from middle frame
+        String text = driver.findElement(By.cssSelector("div#content")).getText();
+        System.out.println(text);
+
+        driver.close();
+    }
+}
