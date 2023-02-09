b958191f5ed5179c91729abdb5a7238b9d5f9647
abhipunj01 <abhishek.punj@dentsu.com>
2023-02-09 22:45:52 +0530

    Removed unused imports

--- src/test/java/Ecommerce.java
+++ src/test/java/Ecommerce.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Ecommerce {
     public static void main(String[] args) {
