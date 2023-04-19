f381c62d2be5332691cfb9d53732f90ae494885d
abhip973 <punj.abhishek1@gmail.com>
2023-04-19 16:52:46 +0530

    Version Update

--- src/test/java/SeleniumTutorials/Assignment2.java
+++ src/test/java/SeleniumTutorials/Assignment2.java
@@ -4,13 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+
+        ChromeOptions options =new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://rahulshettyacademy.com/angularpractice/");
 
